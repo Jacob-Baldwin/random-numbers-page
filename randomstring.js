654f635f7b2f64a075a728e91218238170569fa7
Jacob Baldwin <jacob.ryan.baldwin@gmail.com>
2018-09-19 11:21:55 -0600

    random strings

--- randomstring.js
+++ randomstring.js
@@ -1,1 +1,16 @@
-// computes a random string of a given length
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function getRandomString(length) {
+  var i;
+  var output = "";
+  for (i = 0; i < length; i++) {
+    output = output + chars[getRandomInt(0, 26)];
+  }
+  return output;
+}
+
+function handleRandomStringButton() {
+  let input_length = document.getElementById('RandomStringLength').value;
+  input_length = +input_length;
+  document.getElementById('RandomStringOutput').innerHTML = getRandomString(input_length);
+}
